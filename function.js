6368a024d4fadc5c0aaecec3d990f8ba665805bb
Jasmeet Singh Kainth <43501139+JSM33T@users.noreply.github.com>
2021-01-19 13:00:04 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,25 @@
+      
+        
+		function execcustom() {
+									if ($('#filename').val() != '') execFilenamecustom();
+							  }
+        function execFilenamecustom() 
+				{
+					var filename = encodeURIComponent($('#filename').val());
+					var act = encodeURIComponent($('#act').val());
+					var ext2 = encodeURIComponent($('#ext2').val());
+					var newUrl = 'https://www.google.com/search?q=-inurl%3Ahtm+-inurl%3Ahtml+intitle%3A"index+of"+' + filename + " (" + act +" " + ext2 + ") -html -htm -php -asp -jsp"+ '&oq=-inurl%3Ahtm+-inurl%3Ahtml+intitle%3A"index+of"+' + filename;
+                  window.location.href = newUrl;
+				}
+ 
+              document.getElementById("filename").addEventListener("keydown", function(e)
+              {
+                  if (!e) { var e = window.event; }
+         
+                  
+                  if (e.keyCode == 13)
+                  {
+                      execFilename();
+                  }
+              }, false);    
+
