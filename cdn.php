2f09a2a070ac0e83c08b11c1902b97c4d27ab1c0
Jasmeet Singh Kainth <43501139+JSM33T@users.noreply.github.com>
2021-02-24 14:24:44 +0530

    Update cdn.php

--- cdn.php
+++ cdn.php
@@ -6,12 +6,12 @@
 $css = '
 
 
-<link href="images/favicon.png"rel="shortcut icon">
+
       <link href="https://jsm33t.com/css/bootstrap.min.css"rel="stylesheet">
       <link href="https://jsm33t.com/css/style.css"rel="stylesheet">
       <link href="https://jsm33t.com/css/style-responsive.css"rel="stylesheet">
       <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-      <link href="https://jsm33t.com/css/magnific-popup.css"rel="stylesheet">
+
 
 '; 
 
@@ -22,28 +22,9 @@ $css = '
 $js = '
 
  <script src="https://jsm33t.com/js/jquery-3.5.1.min.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/jquery-migrate-1.4.1.min.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/jquery.easing.1.3.js"type="text/javascript"></script>
+
  <script src="https://jsm33t.com/js/bootstrap.min.js"type="text/javascript"></script>
-  <script src="https://jsm33t.com/js/jquery.scrollTo.min.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/jquery.localScroll.min.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/jquery.viewport.mini.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/jquery.countTo.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/jquery.appear.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/jquery.sticky.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/jquery.parallax-1.1.3.js"type="text/javascript">
- </script><script src="https://jsm33t.com/js/jquery.fitvids.js"type="text/javascript">
- </script><script src="https://jsm33t.com/js/owl.carousel.min.js"type="text/javascript">
- </script><script src="https://jsm33t.com/js/isotope.pkgd.min.js"type="text/javascript">
- </script><script src="https://jsm33t.com/js/imagesloaded.pkgd.min.js"type="text/javascript">
- </script><script src="https://jsm33t.com/js/jquery.magnific-popup.min.js"type="text/javascript">
- </script><script src="https://jsm33t.com/js/wow.min.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/masonry.pkgd.min.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/jquery.simple-text-rotator.min.js"type="text/javascript">
- </script><script src="https://jsm33t.com/js/jquery.lazyload.min.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/all.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/contact-form.js"type="text/javascript"></script>
- <script src="https://jsm33t.com/js/jquery.ajaxchimp.min.js"type="text/javascript"></script>
+
 
 ';
 
