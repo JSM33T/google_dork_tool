d58ada6be922796a3bfeb402fe8d602c4358c7e9
Jasmeet Singh Kainth <43501139+JSM33T@users.noreply.github.com>
2021-02-24 14:17:31 +0530

    Update and rename function.js to dorker.js

--- dorker.js
+++ dorker.js
@@ -1,16 +1,16 @@
       
-        
 		function execcustom() {
 									if ($('#filename').val() != '') execFilenamecustom();
 							  }
         function execFilenamecustom() 
-				{
-					var filename = encodeURIComponent($('#filename').val());
-					var act = encodeURIComponent($('#act').val());
-					var ext2 = encodeURIComponent($('#ext2').val());
-					var newUrl = 'https://www.google.com/search?q=-inurl%3Ahtm+-inurl%3Ahtml+intitle%3A"index+of"+' + filename + " (" + act +" " + ext2 + ") -html -htm -php -asp -jsp"+ '&oq=-inurl%3Ahtm+-inurl%3Ahtml+intitle%3A"index+of"+' + filename;
-                  window.location.href = newUrl;
-				}
+					{
+						var filename = encodeURIComponent($('#filename').val());
+						var act = encodeURIComponent($('#act').val());
+						var ext2 = encodeURIComponent($('#ext2').val());
+						var newUrl = 'https://www.google.com/search?q=-inurl%3Ahtm+-inurl%3Ahtml+intitle%3A"index+of"+' + filename + " (" + act +" " + ext2 + ") -html -htm -php -asp -jsp"+ '&oq=-inurl%3Ahtm+-inurl%3Ahtml+intitle%3A"index+of"+' + filename ;
+					
+						window.open(newUrl); 
+					}
  
               document.getElementById("filename").addEventListener("keydown", function(e)
               {
